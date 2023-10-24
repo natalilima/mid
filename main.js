51a74de84c9752d859302567f10bccd918dd6c31
natalilima <127804567+natalilima@users.noreply.github.com>
2023-10-23 22:16:43 -0300

    alterando

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert("Bora programar galera");
+function somTocaGrilo (){
+    document.querySelector("#som_tecla_grilo").play();
+{
+
+document.querySelector(".tecla_grilo").onclick = somTocaGrilo;
+
+const listaDeTeclas = document.querySelectorA11(".tecla");
+listaDeteclas[4].onclick = tocaSomGrilo;
